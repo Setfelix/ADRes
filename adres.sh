ee4a2fe41c13bdd9ba7edc7575bf722d22b2da3a
Setfelix <felixsetor@yahoo.com>
2015-09-21 19:18:38 +0000

    Added checks for intermediate files

--- adres.sh
+++ adres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Requirements: BWA, sam2fasta.py, abifpy
+# Requirements: BWA, sam2fasta.py, abifpy, fastq-mcf
 
 # Error reporting
 if ! [ "$#" -ge 3 ]; then
@@ -29,23 +29,48 @@ fi
 ./get_fastq_ab1.sh $1 $3
 
 # 2. Filter bases using PHRED quality score
-if [ "$#" -ge 4 ] && [ "$4" -gt 0 ] && [ "$4" -lt 93 ]; then
-    ./ea-utils.1.1.2-806/fastq-mcf n/a $1$3*_$(date +%d_%m_%y).fastq -o $1$3_q$4_$(date +%d_%m_%y).fastq -q $4
+if test ! -e $1$3_$(date +%d_%m_%y); then
+    echo "Error: missing fastq file. Ensure that directory contains .ab1 files."
+    exit 1
 else
-    ./ea-utils.1.1.2-806/fastq-mcf n/a $1$3*_$(date +%d_%m_%y).fastq -o $1$3_q10_$(date +%d_%m_%y).fastq -q 10
+    if [ "$#" -ge 4 ] && [ "$4" -gt 0 ] && [ "$4" -lt 93 ]; then
+        ./ea-utils.1.1.2-806/fastq-mcf n/a $1$3*_$(date +%d_%m_%y).fastq -o $1$3_q$4_$(date +%d_%m_%y).fastq -q $4
+    else
+        ./ea-utils.1.1.2-806/fastq-mcf n/a $1$3*_$(date +%d_%m_%y).fastq -o $1$3_q10_$(date +%d_%m_%y).fastq -q 10
+    fi
 fi
 
 # 2. Align sample sequences to reference coding sequence of same gene
-./bwa-0.7.12/bwa index $2    # index reference sequence
-./bwa-0.7.12/bwa bwasw $2 $1$3_q*$(date +%d_%m_%y).fastq > $1$3_$(date +%d_%m_%y).sam
+if test ! -e $1$3_q*$(date +%d_%m_%y).fastq; then
+    echo "Error: Missing filtered fastq file."
+    exit 1
+else
+    ./bwa-0.7.12/bwa index $2    # index reference sequence
+    ./bwa-0.7.12/bwa bwasw $2 $1$3_q*$(date +%d_%m_%y).fastq > $1$3_$(date +%d_%m_%y).sam
+fi
 
 # 3. Filter reads by mapping quality
-./samtools-1.2/samtools view -Sq 2 $1$3_$(date +%d_%m_%y).sam > $1$3_flt_$(date +%d_%m_%y).sam
+if test ! -e $1$3_$(date +%d_%m_%y).sam; then
+    echo "Error: missing SAM alignment file."
+    exit 1
+else
+    ./samtools-1.2/samtools view -Sq 2 $1$3_$(date +%d_%m_%y).sam > $1$3_flt_$(date +%d_%m_%y).sam
+fi
 
 # 4. Convert SAM alignment to fasta using sam2fasta.py
 #    written by Chang Park available here: http://sourceforge.net/projects/sam2fasta/files/
 #    Usage: sam2fasta.py [ref.fasta] [in.sam] [out.fasta]
-./sam2fasta.py $2 $1$3_flt_$(date +%d_%m_%y).sam $1$3_$(date +%d_%m_%y).fasta
+if test ! -e $1$3_flt_$(date +%d_%m_%y).sam; then
+    echo "Error: missing filtered SAM alignment file."
+    exit 1
+else
+    ./sam2fasta.py $2 $1$3_flt_$(date +%d_%m_%y).sam $1$3_$(date +%d_%m_%y).fasta
+fi
 
 # 5. Parse FASTA alignment to output SNPs at specific codons and their corresponding amino acid changes
-./adr_codons.py -i $1$3_$(date +%d_%m_%y).fasta -g $3
+if test ! -e $1$3_$(date +%d_%m_%y).fasta; then
+    echo "Error: missing FASTA alignment file."
+    exit 1
+else
+    ./adr_codons.py -i $1$3_$(date +%d_%m_%y).fasta -g $3
+fi
